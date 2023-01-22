6a54d71854964289f7c802d5440484955e46ba18
Kevin Peery <kevin.peery@gmail.com>
2023-01-21 22:49:17 -0600

    update mockito testing

--- MockitoTesting.java
+++ MockitoTesting.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class MockitoTesting {
 
     public class educationServiceTest {
@@ -22,19 +25,19 @@ public class MockitoTesting {
     @Test
     public void testStudentsByClass() {
         // Given
-        Student studentJM = new Student(“Joy Ma”, “Spanish);
-        Student studentJH = new Student(“Julio Hernandez”, “Algebra”);
-        Student studentJJ = new Student(“Jenny Jones”, “Calculus”);
+        Student studentJM = new Student("Student1", "Spanish");
+        Student studentJH = new Student("Student2", "Pre-Algebra");
+        Student studentJJ = new Student("Student3", "Calculus2");
         List<Student> allStudents = Arrays.asList(studentJM, studentJH, studentJJ);
 
         given(studentDataObject.getAllStudents()).willReturn(allStudents);
 
         // When
-        List<String> mathStudents = clientBusinessObjectImpl.getAllStudentsBySubject(“math”);
+        List<String> mathStudents = clientBusinessObjectImpl.getAllStudentsBySubject("math");
 
         // Then
         assertThat(mathStudents.size(), is(2));
-        assertThat(mathStudents, hasItems(studentJJ, student JH);
+        assertThat(mathStudents, hasItems(studentJJ, studentJH);
     }
 
 
@@ -42,9 +45,9 @@ public class MockitoTesting {
     public void testMarkInactive() {
 
         // Given
-        Class geometry = new Class(“Geometry”, “Summer 2022);
-        Class envSci = new Class(“Environmental Science”, “Fall 2022”);
-        Class compLit = new Class(“Comparative Literature”, “Spring 2023”);
+        Class geometry = new Class("Geom", "Summer 2022");
+        Class envSci = new Class("Env Science", "Fall 2022");
+        Class compLit = new Class("Comp Lit", "Spring 2023");
         List<Class> allClasses = Arrays.asList(geometry, envSci, compLit);
 
         given(classDataObject.getAllClasses()).willReturn(allClasses);
@@ -75,9 +78,9 @@ public class MockitoTesting {
     @Test
     public void testMarkInactive_argumentCaptor() {
         // Given
-        Class geometry = new Class(“Geometry”, “Summer 2022);
-        Class envSci = new Class(“Environmental Science”, “Fall 2022”);
-        Class compLit = new Class(“Comparative Literature”, “Spring 2023”);
+        Class geometry = new Class("Geom", "Summer 2022");
+        Class envSci = new Class("Env Sci", "Fall 2022");
+        Class compLit = new Class("Comp Lit", "Spring 2023");
         List<Class> allClasses = Arrays.asList(geometry, envSci, compLit);
 
         given(classDataObject.getAllClasses()).willReturn(allClasses);
